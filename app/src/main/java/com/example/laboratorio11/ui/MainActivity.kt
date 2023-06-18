edfe3caa61ac701c13a5ae8bd8ea9e248231d7ad
renegz <00077321@uca.edu.sv>
2023-06-18 00:36:37 -0600

    Add: Labo12

--- app/src/main/java/com/example/laboratorio11/ui/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio11/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.laboratorio11.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.laboratorio11.R
+import com.example.laboratorio11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
